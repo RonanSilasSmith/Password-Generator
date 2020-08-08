25ab8189b5c68bae06c74ff9e15bca71da5cec9c
RonanSilasSmith <smith.ronan@outlook.com>
2020-08-07 17:53:38 -0800

    adds basic logic

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var howMany= function(type, length, maxlength){
+  console.log(length);
+  console.log(maxlength);
+  var output = prompt("How many " + type + " do you want in your password?");
+  output = parseInt(output);
+  if((output + length) > maxlength ){
+    alert("Your input makes the password to long.");
+    howMany(type, length, maxlength);
+  }else{
+    console.log(output, type);
+    return output;
+  }
+ 
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  currentLength = 0;
 
-  passwordText.value = password;
+  passwordLength = prompt("how long do you want your password to be?");
+  passwordLength = parseInt(passwordLength);
+  if (passwordLength >= 8 && passwordLength <= 128){
+    console.log("Password length:" + passwordLength);
+    console.log("current length" + currentLength);
+
+    capitols = howMany("capitol letters", currentLength, passwordLength);
+    currentLength += capitols;
+
+    vowels = howMany("vowels", currentLength, passwordLength);
+    currentLength += vowels;
+
+    numbers = howMany("numbers", currentLength, passwordLength);
+    currentLength += numbers;
+
+    specialChars = howMany("special characters", currentLength, passwordLength);
+    currentLength += specialChars;
+    
+
+    var password = generatePassword(passwordLength, capitols, vowels, numbers, specialChars);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+
+
+
+
+
+  }
+  else{
+    alert("Your password does not meet the length requirements! Enter a number between 8 and 128");
+  }
+
 
 }
 
